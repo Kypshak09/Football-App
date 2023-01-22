e72b5fbceda90daec4b97ae395daf59a17a11895
Kypshak09 <amirzhunusov09@gmail.com>
2023-01-22 20:23:32 +0600

    Initial Commit

---
+++ Alaman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alaman
+//
+//  Created by Amir Zhunussov on 22.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
